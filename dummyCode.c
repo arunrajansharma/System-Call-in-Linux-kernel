cd34fd8cbf728b324377b608595a9b64bdf0dc91
Arun Rajan <arunrajansharma1@hotmail.com>
2017-08-02 21:40:51 -0700

    Create dummyCode.c

---
+++ dummyCode.c
@@ -0,0 +1,12 @@
+#include <asm/unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char* const argv[])
+{
+  printf("Please email me at %s\n", arrajan@cs.stonybrook.edu);
+}
